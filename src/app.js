a25ab441015288db290bd708bc1c06c228a72699
Oleksandr Honsky <ogonskiy@gmail.com>
2020-05-04 15:10:05 +0300

    configure cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/mongoose') // connect mongoose to DB
 const noteRouter = require('./routers/note')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json()) // parse JSON automatically
 app.use(noteRouter)
 
